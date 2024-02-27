81eafd22a64579996e25097ed5566187a5c5f11c
charjiaman <60327912+charjiaman@users.noreply.github.com>
2024-02-27 09:09:54 -0700

    Update holdclean.py
    remove I before DEMO

--- holdclean.py
+++ holdclean.py
@@ -9,7 +9,7 @@ def main():
     df_new1 = df.loc[
         (df['Owner'] != 'INV-NS') & 
         (df['Owner'] != 'INV-S') & 
-        (df['Owner'] != 'IDEMO') & 
+        (df['Owner'] != 'DEMO') & 
         (df['Work Station'].isin(['91-FLIP','88-NOV DEP', '92-FE CMP', '95-SCRUB','89-SACVD', '81-MET DEP', '99-GOLD ROOM', '87-BE HDP', '90-EDGE TRIM', '97-BOND', '82-WCVD', '83-AST', '91-FE GRIND', '86-FE HDP']))
     ]
     df_new = df_new1[['Lot', 'Product',	'Owner', 'Priority Level',	'Operation', 'Area', 'Work Station', 'Qty', 'Days At', 'Hold Code', 'Hold Message']]
